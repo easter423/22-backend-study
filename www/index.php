f8016a72c8e0cd710ef1ff8d98a487f9d09217c4
20191663 <ihs20191663@gmail.com>
2022-09-20 23:31:11 +0900

    3주차

--- www/index.php
+++ www/index.php
@@ -1,2 +1,28 @@
 <?php
-phpinfo();
+$username=$_POST["username"]??null;
+$userpw=$_POST["userpw"]??null;
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    if ($username=="hasa"&&$userpw="ggy"){
+        $islogin=True;
+    }else{
+        $islogin=False;
+    }
+    
+}
+
+
+// 로그인 관련 로그 작업
+// 로그인 요청 성공/실패 여부 알림 발송
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+<body>
+    <?php
+    require($islogin ? "isLogin.php":"isNotLogin.php")
+    ?>
+</body>
---
+++ www/isNotLogin.php
@@ -0,0 +1,6 @@
+<h1>login needed</h1>
+<form action="" method="POST">
+    <input type="text" name="username" placeholder="아이디" />
+    <input type="password" name="userpw" placeholder="비밀번호" />
+    <button type="submit">로그인</button>
+</form>
