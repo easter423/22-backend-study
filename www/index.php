39a50e6ee111f6e286ac1c02b243874065062566
20191663 <ihs20191663@gmail.com>
2022-09-27 22:33:13 +0900

    constructor destructor

--- www/index.php
+++ www/index.php
@@ -6,9 +6,15 @@ class Human
     private string $name;
     private string $id;
 
-    function set_name(string $name)
+    function __construct(string $name)
     {
-        $this->name = $name;
+        $this->name=$name;
+        echo("Constructing Object $this->name<br/>");
+    }
+
+    function __destruct()
+    {
+        echo("Destructing Object $this->name<br/>");
     }
 
     function get_name(): string
@@ -17,39 +23,12 @@ class Human
     }
 }
 
-class Animal
-{
-    private string $name;
-    private string $id;
-
-    function set_name(string $name)
-    {
-        $this->name = $name;
-    }
-
-    function get_name(): string
-    {
-        return $this->name;
-    }
-}
-
-$easteregg423 = new Human();
-$cat = new Animal();
-
-$easteregg423->set_name('하건영');
-$cat ->set_name('고먐미');
-
-echo($easteregg423->get_name());
-echo($cat->get_name());
+$easteregg423 = new Human('하건영');
+$cat = new Human('고먐미');
 
 $objects=[$easteregg423, $cat];
 
 foreach ($objects as $object){
-    if ($object instanceof Human){
-        echo($object->get_name() . "은 Human입니다.");
-    }else{
-        echo($object->get_name() . "은 Human이 아닙니다.");
-    }
-    
-    echo ("<br/>");
+    $name = $object->get_name();
+    echo("${name}<br/>");
 }
