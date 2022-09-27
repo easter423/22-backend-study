957338b7a9b02c362f06ae8a5addaf13e38ef048
20191663 <ihs20191663@gmail.com>
2022-09-27 23:28:24 +0900

    방명록_객체지향

--- www/index.php
+++ www/index.php
@@ -1,18 +1,31 @@
 <?php
 
+class GuestbookMessage
+{
+    public string $name;
+    public string $message;
+}
+
 $fp = fopen("./guestbook.txt","r") or die("Failed to read file");
 
-while(($line = fgets($fp)) !==false):
-    $name = explode(' ', $line)[0];
-    $message = explode(' ',$line)[1];
+$guest_book_messages=[];
+
+while(($line = fgets($fp)) !==false){
+    $message = new GuestbookMessage();
+    $message->name = explode(' ', $line)[0];
+    $message->message = explode(' ',$line)[1];
+    $guest_book_messages[] = $message;
+}
+
+foreach($guest_book_messages as $guest_book_message):
 ?>
 
-<?= $name ?>님이 방명록을 남기셨습니다. <br/>
-<p>
-    <?= $message ?>
-</p>
-<hr/>
+    <?= $guest_book_message->name ?>님이 방명록을 남기셨습니다. <br/>
+    <p>
+        <?= $guest_book_message->message ?>
+    </p>
+    <hr/>
 
 <?php
-    endwhile
+    endforeach
 ?>
